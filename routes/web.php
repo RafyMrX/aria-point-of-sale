a9324f2640910ebcb213dbad684a61790a44e41a
Ahmad Rafi Akbar Putra Hamzah <rafy.works@gmail.com>
2023-07-05 05:49:59 +0700

    file welcome

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/dashboard',[DashboardController::class, 'index'])->name('dashboard');
 // data supplier
 Route::get('/suppliers',[PagesController::class, 'suppliers'])->name('suppliers');
